52166808810e4bc7d96d03a88a6d0a3f2ea41a34
cydem <852415341@qq.com>
2016-11-30 17:46:32 +0800

    Update 念整数.c

--- 念整数.c
+++ 念整数.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 	int x;int a;int t;
-	scanf("%d",&x); 
-	if(x>=-100000&&x<100000){
+	scanf("%d",&x);
+	if(x>=-100000&&x<100000)
+	{
+		
 		if(x<0){
 			printf("fu ");
-			x=-x;
-		}	
-		switch (a){
-			case 0:printf("ling ");break;
-			case 1:printf("yi ");break;
-			case 2:printf("er ");break;
-			case 3:printf("san ");break;
-			case 4:printf("si ");break;
-			case 5:printf("wu ");break;
-			case 6:printf("liu ");break;
-			case 7:printf("qi ");break;
-			case 8:printf("ba ");break;
-			case 9:printf("jiu ");break;
+			x*=-1;
 		}
-			
-			
+		if(x>0){
+			t=x;
+			int mask = 1;
+			while(t>9)
+			{
+				t/=10;
+				mask*=10;
+			}
+		do{
+			a=x/mask;
+			switch (a){
+			case 0:printf("ling");break;
+			case 1:printf("yi");break;
+			case 2:printf("er");break;
+			case 3:printf("san");break;
+			case 4:printf("si");break;
+			case 5:printf("wu");break;
+			case 6:printf("liu");break;
+			case 7:printf("qi");break;
+			case 8:printf("ba");break;
+			case 9:printf("jiu");break;
+			}
+			if(mask>9)
+			{
+				printf(" ");
+			}x%=mask;
+			 mask/=10;	
+		}while(mask>0);
+		
+	}else{
+		printf("ling");
+	}
 	}
 	return 0;
 }
-
